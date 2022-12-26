8b48990f34096299b70c5b7a036b5fe1084930c6
Marco Quiña <marcoq558@gmail.com>
2022-12-26 18:17:55 -0500

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,7 +17,7 @@ public class Main {
         PrintWriter writer = new PrintWriter("C:\\Users\\mquin\\Downloads\\filename.txt", "UTF-8");
         while((fila = csvReader.readNext()) != null) {
             try {
-                writer.println("(NULL, '"+fila[0]+"', '"+fila[1]+"', '"+fila[2]+"', '"+fila[3]+"'"+"', '"+fila[4]+"', '"+fila[5]+"', '"+fila[6]+"'"+"', '"+fila[7]+"', '"+fila[8]+"', '"+fila[9]+"'"+"', '"+fila[10]+"', current_timestamp(), current_timestamp()),");
+                writer.println("(NULL, '"+fila[0]+"', '"+fila[1]+"', '"+fila[2]+"', '"+fila[3]+"', '"+fila[4]+"', '"+fila[5]+"', '"+fila[6]+"', '"+fila[7]+"', '"+fila[8]+"', '"+fila[9]+"', '"+fila[10]+"', current_timestamp(), current_timestamp()),");
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -25,6 +25,28 @@ public class Main {
         }
         writer.close();
         csvReader.close();
+
+                /*LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println("After formatting: " + formattedDate);*/
+
+        /*String strDatewithTime = "2015-08-04T10:11:30";
+        LocalDateTime aLDT = LocalDateTime.parse(strDatewithTime);
+        System.out.println("Date with Time: " + aLDT);*/
+
+        /*DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss.SSSSSS Z");
+        ZonedDateTime.now().format(FORMATTER);
+
+        String strDatewithTime = "2022-11-21T01:31:16.763652Z";
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(strDatewithTime);
+        System.out.println(zonedDateTime);
+
+        String foo = "soy un texto";
+        String bar = foo. split(" ")[0];
+        System.out.println(bar);*/
     }
 
 }
